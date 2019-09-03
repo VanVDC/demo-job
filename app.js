898da48381404b5710589e3522a3ee288d88fcd8
VanVDC <vanbvy@gmail.com>
2019-09-03 13:25:07 -0400

    change teh syntax and change from ejs to handlebars

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.render('index'));
 
 // Gig routes
-app.use('/gigs', require('./routes/gigs'));
+app.use('/gigs', require('./routes/jobs'));
 
 //Chat route
 
